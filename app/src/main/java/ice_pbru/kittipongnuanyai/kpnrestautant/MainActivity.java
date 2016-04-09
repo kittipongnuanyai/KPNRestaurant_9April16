e9b6f1c3aa3cc3c7854b7e59fd23e2c281a93f83
Kittipong <kittipong.nuanyai@gmail.com>
2016-04-09 14:46:18 +0700

    syy userTABLE finish

--- app/src/main/java/ice_pbru/kittipongnuanyai/kpnrestautant/MainActivity.java
+++ app/src/main/java/ice_pbru/kittipongnuanyai/kpnrestautant/MainActivity.java
@@ -1,8 +1,18 @@
 package ice_pbru.kittipongnuanyai.kpnrestautant;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,9 +34,74 @@ public class MainActivity extends AppCompatActivity {
         //Delete SQLite
         deleteSQLite();
 
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
 
 
     }   //Main method
+
+    private void synJSONtoSQLite() {
+
+        MyMainConnected myMainConnected = new MyMainConnected();
+        myMainConnected.execute();
+
+    }
+
+    //create inner class
+    public class MyMainConnected extends AsyncTask<Void, Void, String> {
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/9Apr/php_get_user_kittipong.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+
+
+
+        }// doInBack
+
+        @Override
+        protected void onPostExecute(String strJSON) {
+            super.onPostExecute(strJSON);
+
+            Log.d("Restaurant", "strJSON==>" + strJSON);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+                for (int i = 0 ; i<jsonArray.length();i++ ) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String strUser = jsonObject.getString(MyManage.column_user);
+                    String strPassword = jsonObject.getString(MyManage.column_pass);
+                    String strName = jsonObject.getString(MyManage.column_name);
+
+                    myManage.addValueToSQLite(0, strUser, strPassword, strName);
+
+                } //for
+
+            } catch (Exception e) {
+                Log.d("Restaurant", "Error ==> " + e.toString());
+            }   //try
+
+        }   //onPost
+
+    }//My Main Connected Class
+
+
 
     private void deleteSQLite() {
         SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
